90f6eb618c8679b7cb463dc95391c9b4cb4a3a24
HiepBQ <63699147+Hiepbq2003@users.noreply.github.com>
2026-02-05 00:42:46 +0700

    Time: 2 ms (93.94%), Space: 47.7 MB (47.17%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int n = nums.length;
+        int count[] = new int[n + 1];
+    
+        for(int x : nums){
+            count[x] ++;
+        }
+        int mis = -1 ; int dup = -1;
+        for(int i = 1 ; i <= n ; i ++){
+            if(count[i] == 2) dup = i;
+            if(count[i] == 0) mis = i;
+        }
+        return new int[]{dup , mis};
+    }
+}
