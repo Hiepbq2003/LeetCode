56faace25bf3abbeccfd504034e933b02fce280e
HiepBQ <63699147+Hiepbq2003@users.noreply.github.com>
2026-02-27 00:39:59 +0700

    Time: 17 ms (39.42%), Space: 93.1 MB (67.12%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,15 +1,12 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-              Set<Integer> set = new HashSet<>();
-        
-        for (int num : nums) {
-            if (set.contains(num)) {
-                return true;
-            }
+        HashSet <Integer> set = new HashSet<>();
+
+        for(int num : nums) {
+            if(set.contains(num))
+            return true;
             set.add(num);
         }
-        
-        return false;
-  
+    return false;
     }
 }
