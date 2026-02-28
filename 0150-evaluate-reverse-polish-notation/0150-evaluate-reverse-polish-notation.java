1d3a1cd28e7ace90485071a61c5de7948559a74f
HiepBQ <63699147+Hiepbq2003@users.noreply.github.com>
2026-03-01 01:17:38 +0700

    Time: 6 ms (90.17%), Space: 45.6 MB (30.66%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,38 +1,35 @@
-import java.util.Stack;
-
-public class Solution {
+class Solution {
     public int evalRPN(String[] tokens) {
         Stack<Integer> stack = new Stack<>();
+        for(String token : tokens){
+            if("+".equals(token) || "-".equals(token) ||
+            "*".equals(token) || "/".equals(token)  ){
+                
+                int b = stack.pop();
+                int a = stack.pop();
+                
+                switch(token){
+                    case "+" :
+                    stack.push(a + b);
+                    break;
 
-        for (String token : tokens) {
-            // Kiểm tra xem token có phải là toán tử không
-            if ("+".equals(token) || "-".equals(token) || "*".equals(token) || "/".equals(token)) {
-                // Lấy 2 toán hạng ra khỏi stack
-                int b = stack.pop(); // Toán hạng sau
-                int a = stack.pop(); // Toán hạng trước
+                    case "-" :
+                    stack.push(a - b);
+                    break;
 
-                switch (token) {
-                    case "+":
-                        stack.push(a + b);
-                        break;
-                    case "-":
-                        stack.push(a - b);
-                        break;
-                    case "*":
-                        stack.push(a * b);
-                        break;
-                    case "/":
-                        // Phép chia trong Java mặc định làm tròn về 0 với kiểu int
-                        stack.push(a / b);
-                        break;
+                    case "*" :
+                    stack.push(a * b);
+                    break;
+
+                    case "/" :
+                    stack.push(a / b);
+                    break;
                 }
-            } else {
-                // Nếu là số, đổi từ String sang Integer và đẩy vào stack
+            }
+            else{
                 stack.push(Integer.parseInt(token));
             }
         }
-
-        // Kết quả cuối cùng nằm ở đỉnh stack
         return stack.pop();
     }
 }
