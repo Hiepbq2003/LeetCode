491f8bd8509078465bbed5eeb7ff211e5e8677a7
HiepBQ <63699147+Hiepbq2003@users.noreply.github.com>
2026-03-05 00:30:34 +0700

    Time: 23 ms (81.22%), Space: 90.5 MB (48.89%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,18 +1,13 @@
-import java.util.HashMap;
-
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        
-        for (int i = 0; i < nums.length; i++) {
-            if (map.containsKey(nums[i])) {
-                if (i - map.get(nums[i]) <= k) {
-                    return true;
-                }
+        Map<Integer , Integer> map = new HashMap<>();
+
+        for(int i = 0 ; i < nums.length ; i ++){
+            if(map.containsKey(nums[i])){
+                if(i - map.get(nums[i]) <= k) return true;
             }
-            map.put(nums[i], i); // cập nhật vị trí mới nhất
+            map.put(nums[i] , i);
         }
-        
         return false;
     }
 }
