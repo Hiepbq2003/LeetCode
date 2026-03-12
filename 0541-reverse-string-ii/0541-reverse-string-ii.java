33d2746a1d1f8cce618bbe9ebc88ce7a27e23034
HiepBQ <63699147+Hiepbq2003@users.noreply.github.com>
2026-03-13 01:01:40 +0700

    Time: 1 ms (94.44%), Space: 44.9 MB (43.35%) - LeetHub

--- 0541-reverse-string-ii/0541-reverse-string-ii.java
+++ 0541-reverse-string-ii/0541-reverse-string-ii.java
@@ -1,22 +1,18 @@
 class Solution {
     public String reverseStr(String s, int k) {
-         char[] arr = s.toCharArray();
+        StringBuilder rs = new StringBuilder();
 
-    for (int i = 0; i < arr.length; i += 2 * k) {
+        for(int i = 0 ; i < s.length(); i += 2 * k){
+            int end1 = Math.min(i + k , s.length());
+            String s1 = s.substring(i , end1);
+            rs.append(new StringBuilder(s1).reverse());
 
-        int left = i;
-        int right = Math.min(i + k - 1, arr.length - 1);
-
-        // reverse k characters
-        while (left < right) {
-            char temp = arr[left];
-            arr[left] = arr[right];
-            arr[right] = temp;
-            left++;
-            right--;
-        }
-    }
-
-    return new String(arr);
+            int end2 = Math.min(i + 2 * k , s.length());
+            if(i + k < s.length()){
+                String s2 = s.substring(i + k , end2);
+                rs.append(s2);
+            }
+        }    
+        return rs.toString();
     }
 }
