ef77b0854264e9d5abc416a5eb3e1ac6fc579177
HiepBQ <63699147+Hiepbq2003@users.noreply.github.com>
2026-03-31 19:45:01 +0700

    Time: 2 ms (98.97%), Space: 43.9 MB (58.78%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String addStrings(String num1, String num2) {
+    StringBuilder res = new StringBuilder();
+    int carry = 0 ;
+    int n1 = num1.length() - 1;
+    int n2 = num2.length() - 1;
+    while(n1 >= 0 || n2 >= 0 || carry != 0){
+        int s1 = (n1 >= 0) ? num1.charAt(n1) - '0' : 0;
+        int s2 = 0;
+        if(n2 >= 0){
+            s2 = num2.charAt(n2) - '0';
+        }
+       
+       int sum = s1 + s2 + carry;
+       carry = sum/10;
+        res.append(sum%10);
+        n1--;
+        n2--;
+    }
+    return res.reverse().toString();
+    }
+}
