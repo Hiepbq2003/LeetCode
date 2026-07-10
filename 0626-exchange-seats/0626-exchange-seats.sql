de3cf46803665221cfb2aef721bccd3b89700131
HiepBQ <63699147+Hiepbq2003@users.noreply.github.com>
2026-07-10 23:22:30 +0700

    Time: 339 ms (91.04%), Space: 0B (100%) - LeetHub

---
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -0,0 +1,14 @@
+# Write your MySQL query statement below
+Select 
+    case 
+
+    when id % 2 = 1 and id = (select count(*) from Seat) then id
+
+    when id % 2 = 0 then id - 1
+
+    else id + 1
+
+    END as id , student 
+
+from Seat 
+order by id asc
